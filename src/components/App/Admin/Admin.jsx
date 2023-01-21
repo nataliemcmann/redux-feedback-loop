44875d5ca92b9aeac626ac6b0f7291e567e89b52
Natalie McMann <nmcmann110@gmail.com>
2023-01-21 11:46:07 -0600

    removed unused import code from Admin.jsx

--- src/components/App/Admin/Admin.jsx
+++ src/components/App/Admin/Admin.jsx
@@ -1,7 +1,4 @@
 import React from 'react';
-import axios from 'axios';
-import { useEffect } from 'react';
-import { useDispatch } from 'react-redux';
 
 //import component
 import FeedbackTable from './FeedbackTable';
