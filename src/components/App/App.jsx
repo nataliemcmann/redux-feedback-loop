7745919174a8e877d118c253ba960acbfd45cad4
Natalie McMann <nmcmann110@gmail.com>
2023-01-21 09:20:05 -0600

    added redux and router boilerplate to App.jsx and redux store to  index.js

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,22 +1,32 @@
 import React from 'react';
 import axios from 'axios';
 import './App.css';
-//import hashrouter
-//redux boilerplate
+import { useDispatch } from 'react-redux';
+import { HashRouter as Router, Route, Link, HashRouter } from 'react-router-dom';
+
+//import components
+import Feeling from './Feeling/Feeling.jsx';
 
 function App() {
-  //retrieve feedback reducer from store
+  //declare dispatch object
+  const dispatch = useDispatch();
 
-  //make get function to dispatch database data to store
+  //declare useEffect();
+
+  //
   
   //add exact paths to components
   return (
-    <div className='App'>
-      <header className='App-header'>
-        <h1 className='App-title'>Feedback!</h1>
-        <h4>Don't forget it!</h4>
-      </header>
-    </div>
+    <Router>
+      <div className='App'>
+        <header className='App-header'>
+          <h1 className='App-title'>Feedback!</h1>
+        </header>
+        <Route exact path= "/feeling">
+          <Feeling />
+        </Route>
+      </div>
+    </Router>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -4,12 +4,50 @@ import './index.css';
 import App from './components/App/App';
 import registerServiceWorker from './registerServiceWorker';
 //import REDUX
+import { createStore, combineReducers, applyMiddleware } from 'redux';
+import { Provider } from 'react-redux';
+import logger from 'redux-logger';
 
 //make reducers
+const feeling = (state = 0, action) => {
+    if (action.type === 'SET_FEELING') {
+        return state + action.payload;
+    }
+    return state;
+}
+
+const understanding = (state = 0, action) => {
+    if (action.type === 'SET_UNDERSTANDING') {
+        return state + action.payload;
+    }
+    return state;
+}
+
+const support = (state = 0, action) => {
+    if (action.type === 'SET_SUPPORT') {
+        return state + action.payload;
+    }
+    return state;
+}
+
+const comments = (state = [], action) => {
+    if (action.type === 'SET_COMMENT') {
+        return [action.payload];
+    }
+    return state;
+}
 
 //make the store
+const reduxStore = createStore(combineReducers({
 
+}), applyMiddleware(logger));
 
 //wrap App in boilerplate
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+    <React.StrictMode>
+        <Provider store={reduxStore}>
+            <App />, 
+        </Provider>
+    </React.StrictMode>
+, document.getElementById('root'));
 registerServiceWorker();
